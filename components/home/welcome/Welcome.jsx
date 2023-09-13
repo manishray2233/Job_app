4606e233b61a39cf9b313a808945d73dc10e8acd
Manish Kumar Ray <manishray2233@gmail.com>
2023-09-13 16:30:05 +0530

    Done with Welcome UI

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -8,11 +8,15 @@ import {
   FlatList
 } from 'react-native'
 import { useRouter } from 'expo-router'
+
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants'
 
+const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setactiveJobType] = useState("Full-time")
 
   return (
     <View>
@@ -24,9 +28,40 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
+            value=''
+            onChange={() => { }}
+            placeholder='What are you looking for?'
           />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
       </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.tab(activeJobType, item)} onPress={() => {
+              setactiveJobType(item);
+              router.push(`/search/${item}`)
+            }}>
+              <Text style={styles.tabText(activeJobType, item)}>
+                {item}
+              </Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+
     </View>
   )
 }
