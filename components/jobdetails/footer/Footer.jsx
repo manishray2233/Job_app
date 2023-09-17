c748d93c1e38c0cf99f871f995f964c3a64166c5
Manish Kumar Ray <manishray2233@gmail.com>
2023-09-17 18:43:45 +0530

    Working on Footer

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Linking } from 'react-native'
 
 import styles from './footer.style'
+import { icons } from "../../../constants"
 
-const Footer = () => {
+const Footer = ({ url }) => {
   return (
     <View>
       <Text>Footer</Text>
